12008714ffc28dcdc2b802b4241d73a9880f803c
Bilal Zahid <88459741+bilalzhd@users.noreply.github.com>
2022-11-14 10:56:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const restartBtn = document.querySelector('.restart');
 
 const winCombinations = [
     [0, 1, 2],
-    [0, 3, 6],
+    [3, 4, 5],
     [6, 7, 8],
-    [2, 5, 8],
+    [0, 3, 6],
     [1, 4, 7],
+    [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
 ]
